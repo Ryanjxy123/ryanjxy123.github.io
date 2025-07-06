0bc7748744daebf965046eadc636d9b417a82854
Ryanjxy123 <ryanjxy998244353@gmail.com>
2025-07-06 18:05:47 +0800

    ooo change

--- astro.config.js
+++ astro.config.js
@@ -3,25 +3,24 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
-  // 请修改为你自己的线上地址，谢谢茄子
   site: 'https://ryanjxy123.github.io',
-
-  // 如果你的网站在子路径下（例如 `https://example.com/koi/`），则填写 `/koi/`
-  // 在根路径下（例如 `https://example.com/`）则填写 `/`
   base: '/',
-
   integrations: [mdx(), sitemap(), svelte()],
-
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     remarkRehype: {
       footnoteLabel: "脚注",
       footnoteBackLabel: '文档内容的脚注',
+      remarkPlugins: [remarkMath], // 告诉 Astro 使用 remark-math 处理数学语法
+      rehypePlugins: [rehypeKatex], // 告诉 Astro 使用 rehype-katex 渲染 KaTeX
     }
   },
-
   vite: {
     plugins: [tailwindcss()]
   }

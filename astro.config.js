907bb3fc54f03f0f9fbf670ba3de0023e18bd641
Ryanjxy123 <ryanjxy998244353@gmail.com>
2025-09-22 00:59:59 +0800

    change vercel cfg

--- astro.config.js
+++ astro.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static'; // 静态站点用这个
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import svelte from "@astrojs/svelte";
@@ -7,19 +8,20 @@ import tailwindcss from "@tailwindcss/vite";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
-// https://astro.build/config
+// https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
   site: 'https://ryanjxy123.github.io',
-  base: '/',
+  base: '/',   // 如果你的站点不是部署在独立域名，而是 GitHub Pages 子路径，注意这里要改
+  output: 'static', // 确保是静态构建
+  adapter: vercel(), // <<< 一定要加上这一行！！！
+
   integrations: [mdx(), sitemap(), svelte(), react()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
     remarkRehype: {
       footnoteLabel: "脚注",
-      footnoteBackLabel: '文档内容的脚注',
-      remarkPlugins: [remarkMath], // 告诉 Astro 使用 remark-math 处理数学语法
-      rehypePlugins: [rehypeKatex], // 告诉 Astro 使用 rehype-katex 渲染 KaTeX
+      footnoteBackLabel: '文档内容的脚注'
     }
   },
   vite: {

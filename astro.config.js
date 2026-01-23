99c5b8563b466932e0e29051e0d3a3f545e71249
Ryanjxy123 <ryanjxy998244353@gmail.com>
2026-01-24 00:22:23 +0800

    增添了点击段落标题跳转功能

--- astro.config.js
+++ astro.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import rehypeSlug from 'rehype-slug';
 import sitemap from '@astrojs/sitemap';
 import svelte from "@astrojs/svelte";
-import react from '@astrojs/react';  
+import react from '@astrojs/react';
 import tailwindcss from "@tailwindcss/vite";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
@@ -14,12 +15,12 @@ export default defineConfig({
   integrations: [mdx(), sitemap(), svelte(), react()],
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
+    rehypePlugins: [rehypeKatex, rehypeSlug],
     remarkRehype: {
       footnoteLabel: "脚注",
       footnoteBackLabel: '文档内容的脚注',
       remarkPlugins: [remarkMath], // 告诉 Astro 使用 remark-math 处理数学语法
-      rehypePlugins: [rehypeKatex], // 告诉 Astro 使用 rehype-katex 渲染 KaTeX
+      rehypePlugins: [rehypeKatex, rehypeSlug], // 告诉 Astro 使用 rehype-katex 渲染 KaTeX
     }
   },
   vite: {

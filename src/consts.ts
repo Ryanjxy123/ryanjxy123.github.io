6c33580208c18e9e16f4050afedce77926e34650
Ryanjxy123 <ryanjxy998244353@gmail.com>
2026-03-02 00:30:27 +0800

    update tool page

--- src/consts.ts
+++ src/consts.ts
@@ -28,6 +28,7 @@ export const SITE_COPYRIGHT_YEAR_START = "2024";
 export const SITE_MENU: { title: string, href: string, target: string }[] = [
     { title: "博客", href: "./blog/", target: "" },
     { title: "归档", href: "./page/archive/", target: "" },
+    { title: "工具", href: "./tools/", target: "" },
     { title: "链接", href: "./page/links/", target: "" }
     // { title: "外部链接", href: "https://www.example.com", target: "_blank" }
 ]
